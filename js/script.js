ea1212fc13a70b3e1dbcd02693620a5b518e3d73
zarkarlberal15 <zarkarlberal15@gmail.com>
2021-04-23 17:43:23 +0530

    about me navbar color change when scrolled

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-var clockId = document.getElementById("clock");
-    setInterval(myClock, 1000);
+// var clockId = document.getElementById("clock");
+//     setInterval(myClock, 1000);
   
-    function myClock() {
-      var time = new Date();
-      clockId.innerHTML = time.toLocaleTimeString();
+//     function myClock() {
+//       var time = new Date();
+//       clockId.innerHTML = time.toLocaleTimeString();
+//     }
+
+var myNav = document.getElementById('navBackground');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 1 || document.documentElement.scrollTop >= 1  ) {
+        myNav.classList.add("bg-dark");
+       
+    } 
+    else {
+       
+        myNav.classList.remove("bg-dark");
     }
+};
